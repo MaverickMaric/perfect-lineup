dd9bf2f245edc7a9082a07c1f96076da3b122c70
Maric Brandi <theoneandonlymandarr@gmail.com>
2020-11-01 17:22:50 -0500

    add teamCountCheck on max 2 players/team

--- validateLineup.js
+++ validateLineup.js
@@ -1,14 +1,51 @@
 function validateLineup(lineup) {
   let salaries = []
+  let teams = []
+  let position = []
 
   for (let i = 0; i < lineup.length; i++) {
     salaries.push(lineup[i].salary)
+    teams.push(lineup[i].teamId)
+    position.push(lineup[i].position)
   }
   const teamSalary = salaries.reduce(function (result, item) {
     return result + item
   }, 0)
 
-  if (teamSalary <= 45000) {
+  const teamCounts = teams.reduce(function (obj, team) {
+    if (!obj[team]) {
+      obj[team] = 1
+    } else {
+      obj[team]++
+    }
+
+    return obj
+  }, {})
+
+  let teamCountCheck = true
+
+  for (let team in teamCounts) {
+    if (teamCounts[team] > 2) {
+      teamCountCheck = false
+    }
+  }
+
+  const positionCounts = position.reduce(function (obj, pos) {
+    if (!obj[pos]) {
+      obj[pos] = 1
+    } else {
+      obj[pos]++
+    }
+
+    return obj
+  }, {})
+
+
+  console.log(teamCounts)
+  console.log(positionCounts)
+  console.log(teamCountCheck)
+
+  if (teamSalary <= 45000 && teamCountCheck) {
     return true
   }
 
